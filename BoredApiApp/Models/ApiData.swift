e614eac01195acf21583cb6733de0907dc5e6318
maximsamus <maximsamus1@gmail.com>
2022-04-09 19:14:55 +0300

    UI

---
+++ BoredApiApp/Models/ApiData.swift
@@ -0,0 +1,16 @@
+//
+//  ApiData.swift
+//  BoredApiApp
+//
+//  Created by Максим Самусь on 09.04.2022.
+//
+
+struct Data {
+    let activity: String?
+    let type: String?
+    let participants: Int?
+    let price: Int?
+    let link: Int?
+    let key: Int?
+    let accessibility: Int?
+}
