ae99a2a5806f230d2d1920dc0625c7ae7d5f76a0
maximsamus <maximsamus1@gmail.com>
2022-04-09 18:24:31 +0300

    Initial Commit

---
+++ BoredApiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BoredApiApp
+//
+//  Created by Максим Самусь on 09.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
