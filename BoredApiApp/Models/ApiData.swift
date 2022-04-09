b214b4e062fb438eae3e92f1ba0c924c435b112f
maximsamus <maximsamus1@gmail.com>
2022-04-10 01:16:06 +0300

    Json

--- BoredApiApp/Models/ApiData.swift
+++ BoredApiApp/Models/ApiData.swift
@@ -5,26 +5,32 @@
 //  Created by Максим Самусь on 09.04.2022.
 //
 
-struct Data: Decodable {
-    let activity: String
-    let type: String
-    let participants: Int
-    let price: Double
-    let link: String
-    let key: String
-    let accessibility: Double
+//struct Data: Decodable {
+//    let activity: String
+//    let type: String
+//    let participants: Int
+//    let price: Double
+//    let link: String
+//    let key: String
+//    let accessibility: Double
+//}
+
+struct Scott: Decodable {
+    let name: String?
+    let gender: String?
+    let probability: Float?
+    let count: Int?
 }
 
-//struct Weather: Decodable {
-//    let month: String
-//    let num: String
-//    let link: String
-//    let year: String
-//    let news: String
-//    let safe_title: String
-//    let transcript: String
-//    let alt: String
-//    let img: String
-//    let title: String
-//    let day: String
-//}
+struct Course: Decodable {
+    let name: String?
+    let imageUrl: String?
+    let number_of_lessons: Int?
+    let number_of_tests: Int?
+}
+
+struct Dog: Decodable {
+    let message: String?
+    let status: String?
+    
+}
--- BoredApiApp/ViewControllers/StartCollectionViewController.swift
+++ BoredApiApp/ViewControllers/StartCollectionViewController.swift
@@ -7,15 +7,34 @@
 
 import UIKit
 
-//private let reuseIdentifier = "cell"
-
 class StartCollectionViewController: UICollectionViewController {
     
-    let dataURL = "https://www.boredapi.com/api/activity"
+    private let dataURL = "https://api.genderize.io/?name=scott"
+    private let testURL = "https://swiftbook.ru//wp-content/uploads/api/api_course"
+    private let dogURL = "https://dog.ceo/api/breeds/image/random"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchTest()
         fetchData()
+        fetchDog()
+    }
+    
+    private func fetchTest() {
+        guard let url = URL(string: testURL) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "No Data")
+                return
+            }
+            do {
+                let data = try JSONDecoder().decode(Course.self, from: data)
+                print(data)
+            } catch let error {
+                print ("ERROR IS HERE!!! \(error.localizedDescription)")
+            }
+        }.resume()
     }
     
     private func fetchData() {
@@ -23,17 +42,37 @@ class StartCollectionViewController: UICollectionViewController {
         
         URLSession.shared.dataTask(with: url) { data, _, error in
             guard let data = data else {
-                print((error?.localizedDescription ?? "No Data"))
+                print(error?.localizedDescription ?? "No Data")
                 return
             }
             do {
-                let data = try JSONDecoder().decode(Data.self, from: data)
+                let data = try JSONDecoder().decode(Scott.self, from: data)
                 print(data)
             } catch let error {
                 print ("ERROR IS HERE!!! \(error.localizedDescription)")
             }
         }.resume()
     }
+    
+    
+    private func fetchDog() {
+        guard let url = URL(string: dataURL) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "No Data")
+                return
+            }
+            do {
+                let data = try JSONDecoder().decode(Dog.self, from: data)
+                print(data)
+            } catch let error {
+                print ("ERROR IS HERE!!! \(error.localizedDescription)")
+            }
+        }.resume()
+    }
+    
+    
     
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -47,54 +86,11 @@ class StartCollectionViewController: UICollectionViewController {
         
         return cell
     }
-    
-    
-    // MARK: - UICollectionViewDelegate
-    
-    
-    
-    // MARK: UICollectionViewDelegate
-    
-    /*
-     // Uncomment this method to specify if the specified item should be highlighted during tracking
-     override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-     return true
-     }
-     */
-    
-    /*
-     // Uncomment this method to specify if the specified item should be selected
-     override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-     return true
-     }
-     */
-    
-    /*
-     // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-     override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-     return false
-     }
-     
-     override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-     return false
-     }
-     
-     override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-     
-     }
-     */
-    
 }
 // MARK: - UICollectionViewDelegateFlowLayout
+
 extension StartCollectionViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         CGSize(width: UIScreen.main.bounds.width - 48, height: 100)
     }
 }
-
-// MARK: - Networking
-
-//private func fetchData() {
-//    guard let url = URL(string: dataURL) else { return }
-//
-//}
