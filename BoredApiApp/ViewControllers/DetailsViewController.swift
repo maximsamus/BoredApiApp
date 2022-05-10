4a3088976350e75f4c100fe1383fcc31b755675b
maximsamus <maximsamus1@gmail.com>
2022-05-10 20:19:42 +0300

    fix

--- BoredApiApp/ViewControllers/DetailsViewController.swift
+++ BoredApiApp/ViewControllers/DetailsViewController.swift
@@ -8,14 +8,20 @@
 import UIKit
 
 class DetailsViewController: UIViewController {
-
     
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var imageView: UIImageView! {
+        didSet {
+            imageView.layer.cornerRadius = imageView.frame.width / 2
+        }
+    }
     var character: Results!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        NetworkManager.shared.fetchImage(from: character.image) { imageData in
+            self.imageView.image = UIImage(data: imageData)
+        }
+        descriptionLabel.text = character.description
     }
-
 }
