f808217bdac7d697a5caf39e28c6de06133b2266
maximsamus <maximsamus1@gmail.com>
2022-05-10 14:27:35 +0300

    Fix logic

--- BoredApiApp/Models/ApiData.swift
+++ BoredApiApp/Models/ApiData.swift
@@ -11,3 +11,74 @@ struct Scott: Decodable {
     let probability: Float?
     let count: Int?
 }
+///////////////////// другая прила
+
+enum Link: String {
+    case rickAndMortyApi = "https://rickandmortyapi.com/api/character"
+}
+
+struct RickAndMorty: Codable {
+    let info: Info?
+    let results: [Results]?
+}
+
+struct Info: Codable {
+    let count: Int?
+    let pages: Int?
+    let next: String?
+    let prev: String?
+}
+
+struct Results: Codable {
+    let id: Int?
+    let name: String?
+    let status: String?
+    let species: String?
+    let type: String?
+    let gender: String?
+    let origin: Origin?
+    let location: Loction?
+    let image: String?
+    let episode: [String]?
+    let url: String?
+    let created: String?
+    
+    var description: String {
+        """
+        Name: \(name ?? "")
+        Status: \(status ?? "")
+        Species: \(species ?? "")
+        Type: \(type ?? "")
+        Gender: \(gender ?? "")
+        Origin: \(origin?.name ?? "")
+        Location: \(location?.name ?? "")
+        Created: \(created ?? "")
+        """
+    }
+}
+
+struct Origin: Codable {
+    let name: String?
+    let url: String?
+}
+
+struct Loction: Codable {
+    let name: String?
+    let url: String?
+}
+
+struct Episode: Codable {
+    let id: Int?
+    let name: String?
+    let air_date: String?
+    let episode: [String]?
+    let created: String?
+    
+    var description: String {
+        """
+       Name: \(name ?? "")
+       Air date: \(air_date ?? "")
+       Created: \(created ?? "")
+       """
+    }
+}
--- BoredApiApp/Services/NetworkManager.swift
+++ BoredApiApp/Services/NetworkManager.swift
@@ -9,29 +9,143 @@ import UIKit
 
 class NetworkManager {
     static let shared = NetworkManager()
+    //    private let dataURL = "https://api.genderize.io/?name=scott"
+    //    private var dataScott: Scott?
     
-    private let dataURL = "https://api.genderize.io/?name=scott"
-    private var dataScott: Scott?
+    private init() {}
     
-    func fetchData(from model: Scott, completion: @escaping () -> Void) {
-        guard let url = URL(string: dataURL) else { return }
+    //    func fetchData(from model: Scott, completion: @escaping () -> Void) {
+    //        guard let url = URL(string: dataURL) else { return }
+    //
+    //        URLSession.shared.dataTask(with: url) { data, _, error in
+    //            guard let data = data else {
+    //                print(error?.localizedDescription ?? "No Data")
+    //                return
+    //            }
+    //            do {
+    //                self.dataScott = try JSONDecoder().decode(Scott.self, from: data)
+    //                print(self.dataScott ?? "")
+    //                //                DispatchQueue.main.async {
+    //                //                    self.collectionView.reloadData()
+    //                //                }
+    //            } catch let error {
+    //                print ("ERROR IS HERE!!! \(error.localizedDescription)")
+    //            }
+    //
+    //        }.resume()
+    //    }
+    
+    func fetchRickAndMorty(from url: String?, with completion: @escaping(RickAndMorty) -> Void) {
+        guard let url = URL(string: Link.rickAndMortyApi.rawValue) else { return }
         
         URLSession.shared.dataTask(with: url) { data, _, error in
             guard let data = data else {
-                print(error?.localizedDescription ?? "No Data")
+                print(error?.localizedDescription ?? "No error description")
                 return
             }
             do {
-                self.dataScott = try JSONDecoder().decode(Scott.self, from: data)
-                print(self.dataScott ?? "")
-                //                DispatchQueue.main.async {
-                //                    self.collectionView.reloadData()
-                //                }
+                let result = try JSONDecoder().decode(RickAndMorty.self, from: data)
+                DispatchQueue.main.async {
+                    print(result)
+                    
+                }
             } catch let error {
-                print ("ERROR IS HERE!!! \(error.localizedDescription)")
+                print(error.localizedDescription)
             }
-            
         }.resume()
     }
-    private init() {}
+    
+    func fetchData(from url: String?, with completion: @escaping(RickAndMorty) -> Void) {
+        guard let url = URL(string: url ?? "") else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            
+            do {
+                let rickAndMorty = try JSONDecoder().decode(RickAndMorty.self, from: data)
+                DispatchQueue.main.async {
+                    completion(rickAndMorty)
+                }
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }.resume()
+    }
+    
+    func fetchImage1 (from url: String?, with completion: @escaping(UIImage) -> Void) {
+        guard let url = URL(string: url ?? "") else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            guard let image = UIImage(data: data) else { return }
+            DispatchQueue.main.async {
+                completion(image)
+            }
+        }
+    }
+    func fetchImage(from url: String?, with completion: @escaping(Data) -> Void) {
+        guard let stringURL = url else { return }
+        guard let imageURL = URL(string: stringURL) else { return }
+        DispatchQueue.global().async {
+            guard let data = try? Data(contentsOf: imageURL) else { return }
+            DispatchQueue.main.async {
+                completion(data)
+            }
+        }
+    }
+    
+    func fetchCharacter(from url: String?, with completion: @escaping(Results) -> Void) {
+        guard let url = URL(string: url ?? "") else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            
+            do {
+                let result = try JSONDecoder().decode(Results.self, from: data)
+                DispatchQueue.main.async {
+                    completion(result)
+                }
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }.resume()
+    }
+    
+    func fetchEpisode(from url: String, completion: @escaping(Result<Episode, NetworkError>) -> Void) {
+        guard let url = URL(string: url) else {
+            completion(.failure(.invalidURL))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                completion(.failure(.noData))
+                print(error?.localizedDescription ?? "no description")
+                return
+            }
+            do {
+                let episode = try JSONDecoder().decode(Episode.self, from: data)
+                DispatchQueue.main.async {
+                    completion(.success(episode))
+                }
+            } catch {
+                completion(.failure(.decodingError))
+            }
+        }.resume()
+    }
+    
+    enum NetworkError: Error {
+        case invalidURL
+        case noData
+        case decodingError
+    }
 }
